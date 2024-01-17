276135c6247b11d307e92ca3ea0611433867294d
1234529-hash <113881713+1234529-hash@users.noreply.github.com>
2024-01-17 11:59:14 +0530

    New CHANGES

--- src/Props/index.js
+++ src/Props/index.js
@@ -2,12 +2,12 @@ import React from "react";
 import Component from "./Component";
 
 const Props = () => {
-  // const name = "Perwez";
-  let city ="Lucknow";
+  const name = "Perwez";
+  // let city ="Lucknow";
   return (
     <div className="flex flex-col gap-6">
-      Name from Parent Component : {city}
-      <Component city={city} />
+      Name from Parent Component : {name}
+      <Component name={name} />
     </div>
   );
 };
